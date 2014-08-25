8f40a8e6b2da337d3fc8b0c490ffcd31941a9f48
Adrian <satttyr@gmail.com>
2014-08-25 22:17:47 +0100

    latest changes

--- packet_capture.py
+++ packet_capture.py
@@ -1,51 +1,39 @@
-'''
-rawSocket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(0x0003))
-rawSocket.bind(("wlan1", 0x0003))
-wireless_list = set()
-device_list = set()
-prev_time = datetime.datetime.now()
-#buffer = [65565]
-while True :
-  pkt = rawSocket.recvfrom(64)[0] 
-  if pkt[18] == "\x80" :
-        if pkt[28:34] not in wireless_list  and ord(pkt[55]) > 0:
-            wireless_list.add(pkt[28:34])
-            mac1 = pkt[28:34].encode('hex').upper()
-            print "AP MAC: %s       SSID: %s " % ((':'.join(mac1[i]+mac1[i+1] for i in range(0, len(mac1),2))), pkt[56:56 +ord(pkt[55])])           
-    
-  if pkt[18] == "\xd4":
-    if pkt[22:28] not in wireless_list and pkt[22:28] not in device_list:
-        device_list.add(pkt[22:28])
-        mac2 = pkt[22:28].encode('hex').upper()
-        last_time = time.strftime("%H:%M:%S")
-        print " %s device MAC : %s " % (last_time, (':'.join(mac2[i]+mac2[i+1] for i in range(0, len(mac2),2)))) 
- '''
-
 import datetime
 import pygame
 import pygame.camera
 import pyshark
-wireless_list = dict()
-device_list = dict()
+wirelessList = dict()
+deviceList = dict()
+interfaceName = 'wlan1'
+cameraName = '/dev/video0'
 
+def checkIfWireless(packet):
+    if packet.wlan.fc == "0x8000" :
+        if (packet.wlan.addr not in wirelessList or (datetime.datetime.now() - wirelessList[packet.wlan.addr]) > datetime.timedelta(minutes=1)):
+            print "Wirelless access point MAC: %s       Name: %s" % (packet.wlan.addr, packet.wlan._all_fields["wlan.bssid"].showname) 
+        wirelessList.update({packet.wlan.addr : datetime.datetime.now()}) 
+
+def checkIfNewDevice(packet):
+    if packet is not None and packet.wlan is not None and hasattr(packet.wlan,'da') and packet.wlan.da != "ff:ff:ff:ff:ff:ff" and packet.wlan.addr is not None and packet.wlan.addr not in wirelessList :
+        captureTime = datetime.datetime.now()
+        if packet.wlan.addr not in deviceList or (datetime.datetime.now() - deviceList[packet.wlan.addr]) > datetime.timedelta(minutes=1):
+                imageName = packet.wlan.addr + "_" + unicode(captureTime) + ".jpg"
+                print " device MAC : %s " % (packet.wlan.addr) 
+                img = cam.get_image()
+                pygame.image.save(img, imageName)
+        deviceList.update({packet.wlan.addr : captureTime})
+                    
 pygame.camera.init()
-camera_list = pygame.camera.list_cameras()
-if (len(camera_list) == 0):
+cameraList = pygame.camera.list_cameras()
+if (len(cameraList) == 0):
     print "No video devices detected"
 else:
-    cam = pygame.camera.Camera("/dev/video0", (640,480))
+    cam = pygame.camera.Camera(cameraName, (640, 480))
     cam.start()
-    capture = pyshark.LiveCapture(interface='wlan1')
+    capture = pyshark.LiveCapture(interface = interfaceName)
     while True :
         for pkt in capture.sniff_continuously(packet_count=1000):
             if hasattr(pkt, 'wlan'):
-                if pkt.wlan.fc == "0x8000" and (pkt.wlan.addr not in wireless_list or (datetime.datetime.now()- wireless_list[pkt.wlan.addr]) > datetime.timedelta(seconds=30) ):
-                    wireless_list.update({pkt.wlan.addr : datetime.datetime.now()})
-                    print "Wirelless device MAC: %s       Name: %s" % (pkt.wlan.addr, pkt.wlan._all_fields["wlan.bssid"].showname)           
-                if pkt.wlan.da != "ff:ff:ff:ff:ff:ff" and pkt.wlan.addr not in wireless_list and (pkt.wlan.addr not in device_list or (datetime.datetime.now()- device_list[pkt.wlan.addr]) > datetime.timedelta(seconds=30)):
-                    capture_time = datetime.datetime.now()
-                    image_name = pkt.wlan.addr + "_" +unicode(capture_time) + ".jpg"
-                    device_list.update({pkt.wlan.addr : capture_time})
-                    print " device MAC : %s " % (pkt.wlan.addr) 
-                    img = cam.get_image()
-                    pygame.image.save(img, image_name)
+                checkIfWireless(pkt)
+                checkIfNewDevice(pkt)                        
+                
